740883d524f7473ab1239d5d724f32909824270d
reginpv <reginpv@gmail.com>
2025-12-02 13:21:10 +0800

    feat: header compo added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Header from '@/app/components/Header'
 
 export default function Home() {
   return (
     <div className="">
+      <Header />
       Hello Bulsu
     </div>
   );
